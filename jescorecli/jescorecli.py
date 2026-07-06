5b3304e985905f907888e55ea02d72ef7abebc58
jake-is-ESD-protected <jt@tschavoll.at>
2026-07-06 23:17:08 +0200

    apply most linting checks

--- jescorecli/jescorecli.py
+++ jescorecli/jescorecli.py
@@ -6,11 +6,21 @@
 # -----------------------------------
 
 import argparse
+import platform
+
 import serial
 from serial.tools import list_ports
-from .common import *
-import platform
-from .import config
+
+from . import config
+from .common import (
+    CLI_PREFIX_CLIENT,
+    CLI_PREFIX_MCU,
+    CLI_VERBOSE_DEBUG_PREFIX,
+    KNOWN_HOSTS,
+    RESPONSE_OK,
+    RESPONSE_TRX_OVER,
+)
+
 
 class CjescoreCli:
     def __init__(self, port, baudrate=115200, verbose=False) -> None:
@@ -18,19 +28,19 @@ class CjescoreCli:
         self.baudrate = baudrate
         self.verbose = verbose
         self.port = port
-    
+
     @classmethod
-    def vPrint(cls, printable, end='\n'):
+    def vprint(cls, printable, end='\n'):
         if config.config_verbose:
             print(CLI_VERBOSE_DEBUG_PREFIX, printable, end=end)
 
     @classmethod
-    def cliPrint(cls, printable, end='\n'):
+    def cliprint(cls, printable, end='\n'):
         if config.config_cli_usage:
             print(printable, end=end)
 
     @classmethod
-    def discoverPorts(cls):
+    def discoverports(cls):
         ports = list(list_ports.comports())
         descriptors = []
         for port in ports:
@@ -40,63 +50,63 @@ class CjescoreCli:
         return descriptors
 
     @classmethod
-    def portAutoDetect(cls):
+    def portautodetect(cls):
         ports = list(list_ports.comports())
         for port in ports:
-            CjescoreCli.vPrint(f"Found device: {port.hwid} on port {port.name}")
+            CjescoreCli.vprint(f"Found device: {port.hwid} on port {port.name}")
             for host in KNOWN_HOSTS.values():
                 if host in port.hwid:
-                    return CjescoreCli.__formatPortForOS(port.name)
+                    return CjescoreCli.__formatportforos(port.name)
         return None
-    
+
     @classmethod
-    def __formatPortForOS(cls, port):
+    def __formatportforos(cls, port):
         """Prefix the port with '/dev/' if the system is Linux."""
         if platform.system() == "Linux" and not port.startswith("/dev/"):
             return f"/dev/{port}"
         return port
-    
-    def uartTransceive(self, msg: str, port: str = None, waitTime: float = 0.01, filter=None, keepOpen=False) -> str:
+
+    def uarttransceive(self, msg: str, port: str = None, waittime: float = 0.01, filt=None, keepopen=False) -> str:
         try:
             port_name = port if port else self.port
-            CjescoreCli.vPrint(f"Sending raw string '{msg}' to jescore on port {port_name}")
-            ser = serial.Serial(port_name, baudrate=self.baudrate, timeout=waitTime)
+            CjescoreCli.vprint(f"Sending raw string '{msg}' to jescore on port {port_name}")
+            ser = serial.Serial(port_name, baudrate=self.baudrate, timeout=waittime)
             ser.flush()
             ser.setRTS(False)
             ser.write(msg.encode())
             stat = ""
             returns = []
-            if keepOpen:
+            if keepopen:
                 while 1:
                     stat = ser.readline().decode('utf-8', errors="ignore").strip("\n\r\x00")
                     if stat != "":
-                        if filter and not any(f in stat for f in filter):
+                        if filt and not any(f in stat for f in filt):
                             continue
-                        CjescoreCli.cliPrint(stat, end=config.config_iteration_print_end)
+                        CjescoreCli.cliprint(stat, end=config.config_iteration_print_end)
             while RESPONSE_TRX_OVER not in stat:
                 stat = ser.readline().decode('utf-8', errors="ignore").strip("\n\r\x00")
                 if stat != "":
                     if RESPONSE_TRX_OVER in stat:
-                        CjescoreCli.vPrint(RESPONSE_OK)
+                        CjescoreCli.vprint(RESPONSE_OK)
                     returns.append(stat)
             if len(returns) != 1:
-                CjescoreCli.cliPrint(CLI_PREFIX_CLIENT)
+                CjescoreCli.cliprint(CLI_PREFIX_CLIENT)
             for s in returns:
                 if CLI_PREFIX_MCU not in s:
-                    if filter and not any(f in s for f in filter):
+                    if filt and not any(f in s for f in filt):
                         continue
-                    CjescoreCli.cliPrint(s)
+                    CjescoreCli.cliprint(s)
             return returns
         except KeyboardInterrupt:
-            CjescoreCli.vPrint(f"Closing port {port_name}.")
+            CjescoreCli.vprint(f"Closing port {port_name}.")
             return
 
-    def run(self, command, filter):
+    def run(self, command, filt):
         if command:
-            stat = self.uartTransceive(command, filter=filter)
-            CjescoreCli.vPrint(f"Received raw string {stat}")
+            stat = self.uarttransceive(command, filt=filt)
+            CjescoreCli.vprint(f"Received raw string {stat}")
         else:
-            CjescoreCli.cliPrint("Error: Command not specified.")
+            CjescoreCli.cliprint("Error: Command not specified.")
 
 
 def main():
@@ -115,34 +125,34 @@ def main():
     config.config_cli_usage = True
 
     if args.discover:
-        descriptors = CjescoreCli.discoverPorts()
+        descriptors = CjescoreCli.discoverports()
         for descriptor in descriptors:
-            CjescoreCli.cliPrint(descriptor)
+            CjescoreCli.cliprint(descriptor)
         return
-    
+
     if not args.port:
-        port = CjescoreCli.portAutoDetect()
+        port = CjescoreCli.portautodetect()
     else:
         port = args.port
     if not port and not args.port:
-        CjescoreCli.cliPrint("No jescore-enabled device detected!")
+        CjescoreCli.cliprint("No jescore-enabled device detected!")
         exit()
     cli = CjescoreCli(baudrate=args.baudrate, port=port, verbose=args.verbose)
 
     if args.filter:
-        filter = args.filter.strip('[]').split(',')
-        filter.append("core")
+        filt = args.filter.strip('[]').split(',')
+        filt.append("core")
     else:
         if args.command:
-            filter = [args.command, "core"]
+            filt = [args.command, "core"]
         else:
-            filter = None
-    
+            filt = None
+
     if args.inline:
         config.config_iteration_print_end = '\r'
 
     command_to_send = ' '.join([args.command] + unknown_args) if args.command else ' '.join(unknown_args)
-    cli.uartTransceive(command_to_send, filter=filter, keepOpen=args.listen)
+    cli.uarttransceive(command_to_send, filt=filt, keepopen=args.listen)
 
 
 if __name__ == "__main__":
